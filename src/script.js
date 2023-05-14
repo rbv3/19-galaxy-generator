0772983dd9307831e5064368c9d0436308cece92
Rodrigo Valenca <rvalenca@amazon.com>
2023-05-14 19:25:10 -0300

    added colors and changed initial parameters

--- src/script.js
+++ src/script.js
@@ -19,13 +19,15 @@ Galaxy
 */
 const parameters = {
     count: 10000,
-    radius: 5,
+    radius: 7,
     size: 0.02,
-    branches: 3,
+    branches: 5,
     spin: 1,
-    randomness: 0.02,
-    randomnessPower: 3,
-    height: 1
+    randomness: 0.25,
+    randomnessPower: 5,
+    height: 3,
+    innerColor: '#ff6030',
+    outColor: '#1b3984'
 };
 
 let geometry = null;
@@ -41,9 +43,14 @@ const generateGalaxy = () => {
     // geometry
     geometry = new THREE.BufferGeometry();
     const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3) // RGB
+
+    const colorInside = new THREE.Color(parameters.innerColor);
+    const colorOutside = new THREE.Color(parameters.outColor);
 
     for(let i=0; i<parameters.count; i++) {
         const i3 = i*3;
+        //position
         const radius = Math.random() * parameters.radius;
         const spingAngle = radius * parameters.spin;
         const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
@@ -55,10 +62,22 @@ const generateGalaxy = () => {
         positions[i3] = Math.cos(branchAngle + spingAngle) * radius + randomX;
         positions[i3+1] = 0 + randomY * parameters.height;
         positions[i3+2] = Math.sin(branchAngle + spingAngle) * radius + randomZ;
+
+        //color
+        const mixedColor = colorInside.clone();
+        mixedColor.lerp(colorOutside, radius / parameters.radius)
+
+        colors[i3] = mixedColor.r;
+        colors[i3+1] = mixedColor.g;
+        colors[i3+2] = mixedColor.b;
     }
     geometry.setAttribute(
         'position',
         new THREE.BufferAttribute(positions, 3)
+    );
+    geometry.setAttribute(
+        'color',
+        new THREE.BufferAttribute(colors, 3)
     );
 
     // material
@@ -66,7 +85,8 @@ const generateGalaxy = () => {
         size: parameters.size,
         sizeAttenuation: true,
         depthWrite: false,
-        blending: THREE.AdditiveBlending
+        blending: THREE.AdditiveBlending,
+        vertexColors: true
     });
 
     // points
@@ -80,13 +100,15 @@ generateGalaxy();
 Gui Updates
 */
 gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy);
-gui.add(parameters, 'radius').min(1).max(10).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, 'radius').min(1).max(20).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, 'size').min(0.01).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, 'spin').min(-5).max(5).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'randomness').min(0).max(2).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'height').min(1).max(10).step(0.1).onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'innerColor').onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'outColor').onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
