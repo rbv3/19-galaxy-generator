e187835d08bddcd9fc6add3cf8512d62221979af
Rodrigo Valenca <rvalenca@amazon.com>
2023-05-14 19:14:56 -0300

    adding randomness to galaxy

--- src/script.js
+++ src/script.js
@@ -18,11 +18,14 @@ const scene = new THREE.Scene()
 Galaxy
 */
 const parameters = {
-    count: 1000,
+    count: 10000,
     radius: 5,
     size: 0.02,
     branches: 3,
-    spin: 1
+    spin: 1,
+    randomness: 0.02,
+    randomnessPower: 3,
+    height: 1
 };
 
 let geometry = null;
@@ -45,9 +48,13 @@ const generateGalaxy = () => {
         const spingAngle = radius * parameters.spin;
         const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-        positions[i3] = Math.cos(branchAngle + spingAngle) * radius;
-        positions[i3+1] = 0;
-        positions[i3+2] = Math.sin(branchAngle + spingAngle) * radius;
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5? 1 : -1) * parameters.randomness;
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5? 1 : -1) * parameters.randomness;
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5? 1 : -1) * parameters.randomness;
+
+        positions[i3] = Math.cos(branchAngle + spingAngle) * radius + randomX;
+        positions[i3+1] = 0 + randomY * parameters.height;
+        positions[i3+2] = Math.sin(branchAngle + spingAngle) * radius + randomZ;
     }
     geometry.setAttribute(
         'position',
@@ -77,6 +84,9 @@ gui.add(parameters, 'radius').min(1).max(10).step(1).onFinishChange(generateGala
 gui.add(parameters, 'size').min(0.01).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, 'spin').min(-5).max(5).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomness').min(0).max(2).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'height').min(1).max(10).step(0.1).onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
